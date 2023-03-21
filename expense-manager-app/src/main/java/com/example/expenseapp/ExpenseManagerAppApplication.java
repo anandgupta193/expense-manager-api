7fd7d2811c42f13a276fd169d09a1b035f4af199
uc-aru <arunoday.kesharwani@ucertify.com>
2023-03-22 02:02:19 +0530

    Adding setup code of spring boot

---
+++ expense-manager-app/src/main/java/com/example/expenseapp/ExpenseManagerAppApplication.java
@@ -0,0 +1,14 @@
+package com.example.expenseapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseManagerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseManagerAppApplication.class, args);
+		System.out.println("*************Arunoday*************");
+	}
+
+}
